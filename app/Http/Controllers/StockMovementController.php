ed28e99f57ebf8f24e104966c6adfc4b1c996516
231712600hengker <164556380+231712600hengker@users.noreply.github.com>
2026-06-15 23:37:13 +0700

    Add stock movement search filter

--- app/Http/Controllers/StockMovementController.php
+++ app/Http/Controllers/StockMovementController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\StockMovement;
+use Illuminate\Http\Request;
 
 class StockMovementController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $stockMovements = StockMovement::with('coalProduct')->latest()->paginate(10);
-        return view('stock-movements.index', compact('stockMovements'));
+        $search = $request->string('search')->toString();
+        $type = $request->string('type')->toString();
+
+        $query = StockMovement::with('coalProduct');
+
+        if ($search !== '') {
+            $query->where('reference_type', 'like', "%{$search}%")
+                ->orWhere('description', 'like', "%{$search}%");
+        }
+
+        if ($type !== '') {
+            $query->where('type', $type);
+        }
+
+        $stockMovements = $query->latest()->paginate(10)->withQueryString();
+
+        return view('stock-movements.index', compact('stockMovements', 'search', 'type'));
     }
 
     public function show(StockMovement $stockMovement)
