4e9b09ef768c3e0babee10aefd149b9995ced49c
231712600hengker <164556380+231712600hengker@users.noreply.github.com>
2026-06-15 23:07:25 +0700

    Register SCM resource routes

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,18 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\CoalProductController;
+use App\Http\Controllers\PurchaseOrderController;
+use App\Http\Controllers\SalesOrderController;
+use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\StockMovementController;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.alias');
 
 Route::resource('suppliers', SupplierController::class);
 Route::resource('customers', CustomerController::class);
 Route::resource('coal-products', CoalProductController::class);
+Route::resource('purchase-orders', PurchaseOrderController::class);
+Route::resource('sales-orders', SalesOrderController::class);
+Route::resource('shipments', ShipmentController::class);
+Route::resource('stock-movements', StockMovementController::class)->only(['index', 'show']);
